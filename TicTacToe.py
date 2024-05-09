ad58e7fe4b9e35d0188a61e4c35b1a54e3d96d73
Parth Govale <133950391+Demolus13@users.noreply.github.com>
2024-05-09 13:34:57 +0530

    chore: Refactor TicTacToe game page in app.py

---
+++ TicTacToe.py
@@ -0,0 +1,167 @@
+"""
+Importing the necessary libraries
+"""
+import numpy as np
+import streamlit as st
+
+# Remove all the warnings
+import warnings
+warnings.filterwarnings('ignore')
+
+"""
+Utilities for the game
+"""
+
+n_board = 3
+
+def board_encoding(board: np.ndarray) -> int:
+    """
+    board: np.ndarray: Playing board as an array
+
+    Return: int: board encoding
+    """
+
+    encode = 0
+    power = 1
+    for row in range(n_board):
+        for col in range(n_board):
+            if board[row, col] == 1:
+                encode += power
+            elif board[row, col] == -1:
+                encode += 2*power
+            
+            power *= n_board
+
+    return encode
+
+def print_board(board: np.ndarray):
+    """
+    board: np.ndarray: Playing board as an array
+    """
+
+    symbols = {0: '--', 1: 'X', -1: 'O'}
+    for i in range(3):
+        cols = st.columns(3)
+        for j in range(3):
+            cols[j].markdown(f"<h3 style='text-align: center;'>{symbols[board[i][j]]}</h3>", unsafe_allow_html=True)
+
+def is_full(board: np.ndarray) -> bool:
+    """
+    board: np.ndarray: Playing board as an array
+
+    Return: bool: if the board is full
+    """
+
+    for row in range(n_board):
+        for col in range(n_board):
+            if board[row, col] == 0:
+                return False
+    return True
+
+def has_won(board: np.ndarray, player: int) -> bool:
+    """
+    board: np.ndarray: Playing board as an array
+    player: int: Numeric encoding
+
+    Return: bool: if player has won
+    """
+
+    # check for vertical
+    for row in range(n_board):
+        for col in range(n_board):
+            if board[row, col] != player:
+                break
+        else:
+            return True
+        
+    # check for horizontal
+    for col in range(n_board):
+        for row in range(n_board):
+            if board[row, col] != player:
+                break
+        else:
+            return True
+        
+    # check for diagonal 1
+    for row in range(n_board):
+        if board[row, row] != player:
+            break
+    else:
+        return True
+    
+    # check for diagonal 2
+    for row in range(n_board):
+        if board[row, n_board - row - 1] != player:
+            return False
+    else:
+        return True
+    
+class TicTacToe():
+    """
+    An Optimal TicTacToe player
+    """
+
+    def __init__(self, board: np.ndarray, player: int):
+        """
+        board: np.ndarray: Playing board as an array
+        player: int: Numeric encoding
+        """
+
+        self.board = board
+        self.player = player
+        self.computed_moves = np.zeros((19681, 4), dtype=np.int32)
+
+    def best_move(self, board: np.ndarray, player: int) -> np.ndarray:
+        """
+        board: np.ndarray: Playing board as an array
+        """
+
+        assert ~is_full(board)
+        assert ~has_won(board, player)
+        assert ~has_won(board, -player)
+
+        no_candidate = 1
+        candidate = np.zeros((1, 4), dtype=np.int32)
+
+        encode = board_encoding(board)
+        if self.computed_moves[encode][0]:
+            return self.computed_moves[encode]
+        
+        for row in range(n_board):
+            for col in range(n_board):
+                if board[row, col] == 0:
+                    board[row, col] = player
+                    if has_won(board, player):
+                        board[row, col] = 0
+                        candidate = np.array([1, row, col, 1])
+                        self.computed_moves[encode] = candidate
+                        return candidate
+                    board[row, col] = 0
+
+        for row in range(n_board):
+            for col in range(n_board):
+                if board[row, col] == 0:
+                    board[row, col] = player
+                    if is_full(board):
+                        board[row, col] = 0
+                        candidate = np.array([1, row, col, 0])
+                        self.computed_moves[encode] = candidate
+                        return candidate
+                    
+                    response = self.best_move(board, -player)
+                    board[row, col] = 0
+                    if response[3] == -1:
+                        candidate = np.array([1, row, col, 1])
+                        self.computed_moves[encode] = candidate
+                        return candidate
+                    elif response[3] == 0:
+                        candidate = np.array([1, row, col, 0])
+                        self.computed_moves[encode] = candidate
+                        no_candidate = 0
+                    elif no_candidate:
+                        candidate = np.array([1, row, col, -1])
+                        self.computed_moves[encode] = candidate
+                        no_candidate = 0
+        
+        self.computed_moves[encode] = candidate
+        return candidate
--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import numpy as np
 import streamlit as st
 from streamlit_option_menu import option_menu
 
+from TicTacToe import is_full, has_won, print_board, TicTacToe
+
 # Set page configuration
 st.set_page_config(page_title="Algorithmic Games",
                    layout="centered")
@@ -23,45 +25,57 @@ if selected == 'TicTacToe':
 
     # page title
     st.title('TicTacToe')
-    st.write('This is a Decison Tree model for Diabetes Prediction')
-    board = np.zeros((3, 3))
-    st.write(board)
+    st.write('Algorithm to play the optimal move in a TicTacToe game')
 
-    # getting the input data from the user
-    col1, col2, col3 = st.columns(3)
-    with col1:
-        Pregnancies = st.text_input('Number of Pregnancies')
-    with col2:
-        Glucose = st.text_input('Glucose Level')
-    with col3:
-        BloodPressure = st.text_input('Blood Pressure value')
-    with col1:
-        SkinThickness = st.text_input('Skin Thickness value')
-    with col2:
-        Insulin = st.text_input('Insulin Level')
-    with col3:
-        BMI = st.text_input('BMI value')
-    with col1:
-        DTreePedigreeFunction = st.text_input('DTree Pedigree Function value')
-    with col2:
-        Age = st.text_input('Age of the Person')
+    # Initialize session_state
+    if 'won' not in st.session_state:
+        st.session_state.won = 0
+    if 'full' not in st.session_state:
+        st.session_state.full = 0
 
+    symbols = {'X': 1, 'O': -1}
+    if 'player' not in st.session_state:
+        st.session_state.player = symbols[st.selectbox('Choose your symbol', options=['X', 'O'])]
+        st.session_state.board = np.zeros((3, 3))
+        st.session_state.computer = TicTacToe(st.session_state.board, -st.session_state.player)
+    else:
+        # Get the current symbol
+        current_player = symbols[st.selectbox('Choose your symbol', options=['X', 'O'])]
 
-    # code for Prediction
-    diab_diagnosis = ''
+        # If the symbol has changed, reset the board
+        if st.session_state.player != current_player:
+            st.session_state.player = current_player
+            st.session_state.board = np.zeros((3, 3))
+            st.session_state.computer = TicTacToe(st.session_state.board, -st.session_state.player)
 
-    # creating a button for Prediction
-    if st.button('Predict'):
-        user_input = [Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin,
-                      BMI, DTreePedigreeFunction, Age]
-        user_input = [float(x) for x in user_input]
-        diab_prediction = 1
-        if diab_prediction[0] == 1:
-            diab_diagnosis = 'The person is diabetic'
-        else:
-            diab_diagnosis = 'The person is not diabetic'
+    if st.session_state.player == 1:
+        # getting the input data from the user
+        col1, col2 = st.columns(2)
+        with col1:
+            row = st.text_input('Enter Row Number (0-2)')
+        with col2:
+            col = st.text_input('Enter Column Number (0-2)')
 
-    st.success(f"Prediction: {diab_diagnosis}")
+        # player's turn
+        if st.button('Play Button') and st.session_state.won == 0:
+            row, col = int(row), int(col)
+            if st.session_state.board[row, col] == 0:
+                st.session_state.board[row, col] = st.session_state.player
+                st.session_state.won = has_won(st.session_state.board, st.session_state.player)
+                st.session_state.full = is_full(st.session_state.board)
+
+            # computers turn
+            if st.session_state.won != 1 and st.session_state.full != 1:
+                response = st.session_state.computer.best_move(st.session_state.board, st.session_state.computer.player)
+                st.session_state.board[response[1], response[2]] = st.session_state.computer.player
+                st.session_state.won = -has_won(st.session_state.board, st.session_state.computer.player)
+        
+        # display the updated board
+        print_board(st.session_state.board)
+
+        # if the game is won
+        if st.session_state.won:
+            st.write(f"{'Player' if st.session_state.won == 1 else 'Computer'} has won!")
 
     # code block
     st.title('Notebook')
